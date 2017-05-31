928a1242f6457ecd927edb9432ff48bc0d3f7710
theClubhou.se <Rebeccavl@outlook.com>
2017-05-31 10:32:13 -0400

    routes update

--- routes/api.php
+++ routes/api.php
@@ -2,15 +2,24 @@
 
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::get('getRoles','RolesController@index');
+Route::post('storeRole','RolesController@store');
+Route::post('updateRole/{id}','RolesController@update');
+Route::get('showRole/{id}','RolesController@show');
+Route::post('deleteRole/{id}','RolesController@destroy');
+
+Route::get('getUsers','UsersController@index');
+Route::post('SignIn','UsersController@SignIn');
+Route::post('SignUp','UsersController@SignUp');
+Route::post('deleteUser/{id}','UsersController@destroy');
+
+Route::post('storeOrder','OrdersController@store');
+Route::get('showOrder/{id}','OrdersController@show');
+Route::post('deleteOrder/{id}','OrdersController@destroy');
+
+Route::post('storeProduct','ProductsController@store');
+Route::post('updateProduct/{id}','ProductsController@update');
+Route::get('showProduct/{id}','ProductsController@show');
+Route::post('deleteProduct/{id}','ProductsController@destroy');
 
 Route::any('{path?}', 'MainController@index')->where("path", ".+");
